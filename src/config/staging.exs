f222ee80ed1feb6df4bdb5de5577d3b44d469ceb
Peiling Ding <dingpl716@gmail.com>
2019-03-08 13:47:10 -0800

    bump version

--- src/config/staging.exs
+++ src/config/staging.exs
@@ -1,1 +1,45 @@
+# use Mix.Config
+
+# config :abt_did_workshop, AbtDidWorkshopWeb.Endpoint,
+#   http: [port: 4000],
+#   debug_errors: true
+
+# # Do not print debug messages in production
+# config :logger, level: :info
+
+# # Configure your database
+# config :abt_did_workshop, AbtDidWorkshop.Repo,
+#   username: "postgres",
+#   password: "postgres",
+#   database: "abt_did_workshop_staging",
+#   hostname: "localhost",
+#   pool_size: 10
+
 use Mix.Config
+
+# For development, we disable any cache and enable
+# debugging and code reloading.
+#
+# The watchers configuration can be used to run external
+# watchers to your application. For example, we use it
+# with webpack to recompile .js and .css sources.
+config :abt_did_workshop, AbtDidWorkshopWeb.Endpoint,
+  http: [port: 4000],
+  debug_errors: true,
+  check_origin: false
+
+# Do not include metadata nor timestamps in development logs
+config :logger, :console, format: "[$level] $message\n"
+# level: :warn
+
+# Set a higher stacktrace during development. Avoid configuring such
+# in production as building large stacktraces may be expensive.
+config :phoenix, :stacktrace_depth, 20
+
+# Configure your database
+config :abt_did_workshop, AbtDidWorkshop.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "abt_did_workshop_staging",
+  hostname: "localhost",
+  pool_size: 10
